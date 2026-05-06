5ae83cea70bd1e5fc4604b2c75655dbb45a72330
noadhum <noa.adhum99@gmail.com>
2026-05-06 07:23:33 +0700

    Working on linear conflict at puzzle.py

--- challenges/artificial-intelligence/n_Puzzle_Solver/puzzle_solver/solver.py
+++ challenges/artificial-intelligence/n_Puzzle_Solver/puzzle_solver/solver.py
@@ -11,12 +11,11 @@ class Node:
 
     Arguments:
     - state: The board state.
-    - move: The move taken to this state.
     - parent: The state's parent.
+    - g: Depth of node/Total move of empty tile (0).
     """
-    def __init__(self, state: List[int], move: Optional[str], parent: Optional[Node], g: int = 0) -> None:
+    def __init__(self, state: List[int], parent: Optional[Node] = None, g: int = 0) -> None:
         self.state = tuple(state)
-        self.move = move
         self.parent = parent
 
         self.g = g
@@ -30,12 +29,15 @@ class Solver:
     Arguments:
     - initial_state: Initial state of the n-Puzzle's game board.
     """
-    def __init__(self, initial_state: List[int]) -> None:
-        self.initial_state = initial_state
-        self.goal_state = list(range(1, len(initial_state))) + [0]
+    def __init__(self, state: List[int]) -> None:
+        self.initial_state = state
+        self.goal_state = list(range(1, len(self.initial_state))) + [0]
 
-        self.open = []
+        self.open: List[Tuple[int, int, Node]] = []
         self.closed: set[Tuple[int, ...]] = set()
+
+        start_node = Node(self.initial_state)
+        heapq.heappush(self.open, (start_node.f, start_node.h, start_node))
     
     def solve(self):
         pass
